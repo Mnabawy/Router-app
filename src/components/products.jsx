af0477defd5e417f45d26dde1e100a10e143e634
Mnabawy <m_ninja61@yahoo.com>
2021-03-28 00:08:04 +0200

    query string parameter

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
 class Products extends Component {
   state = {
     products: [
       { id: 1, name: "Product 1" },
       { id: 2, name: "Product 2" },
-      { id: 3, name: "Product 3" }
-    ]
+      { id: 3, name: "Product 3" },
+    ],
   };
 
   render() {
@@ -15,7 +14,7 @@ class Products extends Component {
       <div>
         <h1>Products</h1>
         <ul>
-          {this.state.products.map(product => (
+          {this.state.products.map((product) => (
             <li key={product.id}>
               <Link to={`/products/${product.id}`}>{product.name}</Link>
             </li>
